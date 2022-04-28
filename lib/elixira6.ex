bdb9f6b5961997ce7aa297a769c7432e161acfa7
Luis Perez <le.perezl@uniandes.edu.co>
2022-04-28 14:40:50 -0500

    stuff

---
+++ lib/elixira6.ex
@@ -0,0 +1,18 @@
+defmodule Elixira6 do
+  @moduledoc """
+  Documentation for `Elixira6`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixira6.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixira6_test.exs
@@ -0,0 +1,8 @@
+defmodule Elixira6Test do
+  use ExUnit.Case
+  doctest Elixira6
+
+  test "greets the world" do
+    assert Elixira6.hello() == :world
+  end
+end
