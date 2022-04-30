d9dbf8cad548be305a7ca457b07e0d32f5df4d20
Luis Perez <le.perezl@uniandes.edu.co>
2022-04-30 17:22:50 -0500

    working image

--- lib/elixira6.ex
+++ lib/elixira6.ex
@@ -1,13 +1,17 @@
-defmodule Elixira6 do
+defmodule Prime do
   import Mogrify
 
-  def resize(imagePath, width, height, opts \\ [path: "C:/Users/User/Desktop/Uniandes/Semestre 8/Concurrencia, paralelismo/elixira6/Fotos"]) do
+  def resize(imagePath, width, height, opts \\ [path: "./Fotos/processed.jpeg"]) do
   open(imagePath)
   |> resize_to_limit(~s(#{width}x#{height}))
   |> save(opts)
   end
 
   def test do
-    System.cmd("cmd.exe", ["magick", "mogrify", "-resize", "20x20", "-write", "C:/Users/User/Desktop/Uniandes/Semestre 8/Concurrencia, paralelismo/elixira6/Fotos/example.jpg", "C:/Users/User/Desktop/Uniandes/Semestre 8/Concurrencia, paralelismo/elixira6/Fotos/image.jpeg"], [stderr_to_stdout: true])
+    System.cmd("cmd.exe", ["magick", "mogrify", "-resize", "20x20", "-write", "./Fotos/example.jpg", "C:/Users/User/Desktop/Uniandes/Semestre 8/Concurrencia, paralelismo/elixira6/Fotos/image.jpeg"], [stderr_to_stdout: true])
+  end
+
+  def linux_test do
+    open("./Fotos/image.jpeg") |> resize_to_limit("20x20") |> save([path: "./Fotos/resized.jpeg"])
   end
 end
