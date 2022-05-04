45cbdff89f1ecaafd9362b7c9ada32263d19f3d6
JulioCesarRuizG <jc.ruizg@uniandes.edu.co>
2022-05-03 22:28:22 -0500

    generar número

--- lib/elixira6.ex
+++ lib/elixira6.ex
@@ -12,6 +12,37 @@ defmodule Image do
   end
 
   def linux_test do
-    open("./Fotos/image.jpeg") |> resize_to_limit("20x20") |> save([path: "./Fotos/resized.jpeg"])
+    open("./Fotos/image.jpeg") |> resize_to_limit("20x20") |> custom("colorspace", "Gray") |> save([path: "./Fotos/resized3.txt"])
+  end
+
+  def process_file do
+    {:ok, content} = File.read("./Fotos/resized3.txt")
+    val = String.split(content, "\r\n", trim: true)
+    process(val, [])
+  end
+
+  def process([h | t], list) do
+    cond do
+      Enum.at(String.split(h, "gray", trim: true),1) != nil and Enum.at(String.split(h, "gray", trim: true),1) != [] ->
+        val = 9-trunc(Float.floor(10*String.to_integer(Enum.at(String.split(Enum.at(String.split(Enum.at(String.split(h, "gray", trim: true),1), "("),1), ")"),0))/255))
+        cond do
+          val < 0 ->
+            list = Enum.concat(list, [0])
+            process(t, list)
+          true ->
+            list = Enum.concat(list, [val])
+            process(t, list)
+        end
+      true ->
+        process(t, list)
+    end
+  end
+
+  def process([], list) do
+    list
+    IO.inspect String.to_integer(Enum.join(list))
+    {:ok, file} = File.open("./Fotos/numbers.txt", [:write])
+    IO.write(file, inspect(list, limit: :infinity))
   end
 end
+#Enum.at(String.split(Enum.at(String.split(Enum.at(String.split(h, "gray", trim: true),1), "("),1), ")"),0)
